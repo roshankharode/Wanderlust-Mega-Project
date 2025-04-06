3b66e67c42227c245b1af785493b64fe418e3ab5
roshankharode <99000074+roshankharode@users.noreply.github.com>
2025-04-06 17:16:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const port = PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({origin: '*',}));
 app.use(cookieParser());
 app.use(compression());
 
